19127109517fce677474ec986f5a7f22e9b318c3
NikollbibajNoah <nikollbibaj.noah@gmail.com>
2025-06-02 16:08:55 +0200

    config

--- explain-my-code-app-frontend/vite.config.ts
+++ explain-my-code-app-frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/explain-my-code-app-frontend/',
+  base: '/explain-my-code-app/',
   plugins: [react()],
 })
